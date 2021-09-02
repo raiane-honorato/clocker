ed725e3db574292867cc1960474a2a1317be8d2c
Raiane Honorato <raianehonorato16@gmail.com>
2021-09-02 07:17:43 -0300

    feat: login form

---
+++ components/index.js
@@ -0,0 +1,2 @@
+//bearer file. um arquivo com todos os exports dos components
+export * from './Logo' 
---
+++ pages/schedule.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Schedule() {
+  return (
+    <div>Teste</div>
+  )
+}
